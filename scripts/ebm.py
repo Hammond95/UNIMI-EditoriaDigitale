bb533273f77f8c292608eb2a8fe0d489bcc21b0a
Hammond95 <martindeluca95@gmail.com>
2022-01-16 02:55:32 +0100

    minor changes

---
+++ scripts/ebm.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#  -*- mode: python; indent-tabs-mode: nil; -*- coding: UTF8 -*-
+
+# Used to run in debug mode
+
+from ebookmaker import EbookMaker
+
+EbookMaker.main()
---
+++ scripts/fonts.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPTDIR"
+
+cat <<'EOT' > /tmp/fontface.def
+@font-face {
+    font-family: "§NAME§";
+    src: url("../fonts/§NAME§.§EXT§");
+    font-style: §STYLE§;
+    font-weight: §WEIGHT§;
+}
+
+EOT
+
+WEIGHTMAP="Bold|bold
+Regular|normal
+Light|200
+Medium|500
+SemiBold|600"
+
+STYLEMAP="Normal|normal
+Italic|italic
+Oblique|oblique"
+
+for file in $(ls -1 ../ebook/assets/fonts); do
+    filename=$(echo $file | cut -d'.' -f1)
+    ext=$(echo $file | cut -d'.' -f2)
+    weightkey=$(echo $filename | cut -d'-' -f2)
+    stylekey=$(echo $filename | cut -d'-' -f3)
+
+    if [ -z "$weightkey" ]; then
+        weight="normal"
+    else
+        weight=$(echo "$WEIGHTMAP" | grep -E "^$weightkey\|" | cut -d '|' -f2)
+    fi
+
+    if [ -z "$stylekey" ]; then
+        style="normal"
+    else
+        style=$(echo "$STYLEMAP" | grep -E "^$stylekey\|"  | cut -d '|' -f2)
+    fi
+
+    cat /tmp/fontface.def | sed "s#§NAME§#$filename#g" | sed "s#§EXT§#$ext#g" | sed "s#§WEIGHT§#$weight#g" | sed "s#§STYLE§#$style#g"
+
+done
