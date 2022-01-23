156e4b7eb85954dff0ab6c9357ff48c77cdd30ab
Hammond95 <martindeluca95@gmail.com>
2022-01-23 01:40:49 +0100

    cleaned up css styles; updated html files to read from minified css.

--- scripts/css-minify.sh
+++ scripts/css-minify.sh
@@ -3,11 +3,16 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPTDIR"
 
-cssfiles=("typography.css" "0.css" "1.css" "pgepub.css")
+cssfiles=("typography.css" "0.css" "1.css" )
 
 echo "" > /tmp/merged-css.css
 for css in "${cssfiles[@]}"; do
     cat "$SCRIPTDIR/../ebook/assets/styles/$css" >> /tmp/merged-css.css
 done
 
-curl -X POST -s --data-urlencode "input@/tmp/merged-css.css" https://www.toptal.com/developers/cssminifier/raw > "$SCRIPTDIR/../ebook/assets/styles/minified/style-min.css"
+curl \
+    -X POST \
+    -s \
+    --data-urlencode "input@/tmp/merged-css.css" \
+    https://www.toptal.com/developers/cssminifier/raw | \
+    sed 's#url("../fonts/#url("../../fonts/#g' > "$SCRIPTDIR/../ebook/assets/styles/minified/style-min.css"
